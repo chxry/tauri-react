74e07361836daf1daf123c8de6a78d9e0be972c3
chxry <thechxry@gmail.com>
2021-03-22 18:04:28 +0000

    First commit

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+import "./index.scss";
+
+// @ts-ignore
+import Hello from "./components/Hello.tsx";
+
+ReactDOM.render(Hello(), document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/app.ts",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    })]
+};
